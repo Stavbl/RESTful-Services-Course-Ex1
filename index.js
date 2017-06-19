0de232a651012904078833cdb7c50ff4c352db4f
StavBl <blstav@gmail.com>
2017-06-19 15:52:58 +0300

    change back

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ var method = methods();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
-app.use((req,res,next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept");
-  res.set("Content-Type", "application/json");
-  next();
-});
-
 app.use(express.static('public'));
 app.get('/', (req,res) => {
   console.log('Trace: API Page');
